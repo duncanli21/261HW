eae0bc29873f8d7f3cf1088823a50b5f1aa7335c
Liam Duncan <duncanli@oregonstate.edu>
2019-04-18 21:09:35 -0700

    check

---
+++ HW2/bag2set.c
@@ -0,0 +1,91 @@
+/* bag2set.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dynArray.h"
+
+
+/* Converts the input bag into a set using dynamic arrays 
+	param: 	da -- pointer to a bag 	
+	return value: void
+        result: after exiting the function da points to a set 		
+*/
+void bag2set(struct DynArr *da)
+{
+   /* struct DynArr* set;*/
+/*     initDynArr(set, da->capacity);*/
+
+        int i,j,k; 
+
+    for(i=0; i<da->size; i++)
+    {
+        for(j=da->size; j>1; j--)
+        {
+                if(da->data[i] == da->data[j])
+                {
+                      /*  for(k=j; k <da->size; k++)
+                        {
+                                da->data[k] = da->data[k+1];
+                        } */
+                        da->size--; 
+                }
+                
+
+        }
+        
+    } 
+
+    /*int i, j; 
+
+    int newlength =da->size; 
+
+    TYPE* 
+
+    for(i=1; i< da->size; i++)
+    {
+            for(j=0; j< newlength; j++)
+            {
+                    if(da->data[i] == da->data[j])
+                    {
+                        da->size--; 
+                        break; 
+                    }
+
+            }
+
+            if(j==newlength)
+            da->data[newlength++] = da->data[i]; 
+    }*/
+}
+
+
+
+/* An example how to test your bag2set() */
+int main(int argc, char* argv[]){
+
+	int i;
+        struct DynArr da;  /* bag */
+        
+        initDynArr(&da, 100);
+        da.size = 10;
+        da.data[0] = 1.3;
+	for (i=1;i<da.size;i++){
+	    da.data[i] = 1.2;
+	 }
+
+        printf("Bag:\n\n");
+	for (i=0;i<da.size;i++){
+          printf("%g  \n", da.data[i]);
+        }
+        
+        printf("\n\n\n");
+        printf("Set:\n\n");
+        bag2set(&da);
+	for (i=0;i<da.size;i++){
+          printf("%g ", da.data[i]);
+        }
+        printf("\n");
+        
+	return 0;	
+}
+
